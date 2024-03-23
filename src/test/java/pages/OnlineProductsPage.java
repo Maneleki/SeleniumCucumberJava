3038fcb4100c67902d03c974714ed2ab7533b224
Maneleki <manelruizblas@gmail.com>
2024-03-23 23:08:13 +0100

    New repository to learn Selenium and Cucumber with Java

--- src/test/java/pages/OnlineProductsPage.java
+++ src/test/java/pages/OnlineProductsPage.java
@@ -7,20 +7,5 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class OnlineProductsPage {
 
-    public static WebDriver driver;
-    public static String hamburger_menu_xpath = "//*[@id=\"menuToggle\"]/input";
-    public static String signIn_link_xpath = "//*[@id=\"menu\"]/a[2]/li";
 
-    public static void click_hamburger_menu() throws InterruptedException {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        driver.get("https://anupdamoda.github.io/AceOnlineShoePortal/index.html#");
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(hamburger_menu_xpath)).click();
-    }
-
-    public static void click_signIn_link() throws InterruptedException {
-        Thread.sleep(2000);
-        driver.findElement(By.xpath(signIn_link_xpath)).click();
-    }
 }
