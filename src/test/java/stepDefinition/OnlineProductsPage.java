5b5c7b0fd03fc57ad0f95a83b9ebc8afb6cd49d0
Maneleki <manelruizblas@gmail.com>
2024-03-27 22:08:57 +0100

    New repository to learn Selenium and Cucumber with Java

--- src/test/java/stepDefinition/OnlineProductsPage.java
+++ src/test/java/stepDefinition/OnlineProductsPage.java
@@ -2,17 +2,25 @@ package stepDefinition;
 
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Assert;
+
+import static org.junit.Assert.assertEquals;
+import static pages.OnlineProductsPage.click_formalshoesdropdown;
+import static pages.OnlineProductsPage.getText_formalshoes_firstvalue;
+import static pages.RegistrationPage.visibility_user_registration_page;
+
 
 
 public class OnlineProductsPage {
 
     @When("User clicks on Formal Shoes dropdown")
     public void user_clicks_on_Formal_Shoes_dropdown() throws InterruptedException{
-
+        click_formalshoesdropdown();
     }
     @Then("User should be able to view the Products")
     public void user_should_be_able_to_view_the_Products() throws InterruptedException{
-
+        getText_formalshoes_firstvalue();
+        assertEquals("   Classic Cheltenham",getText_formalshoes_firstvalue());
     }
 
 }
